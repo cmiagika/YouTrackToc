5591ce92230d8d70fc25f3460679cd8b5176818b
cmiagika <ismail.kassem@cmiag.ch>
2016-07-13 22:58:54 +0200

    :'(

---
+++ YouTrackToc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace YouTrackToc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<TableOfContentService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new TableOfContentService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                });
+                serviceConfig.SetServiceName("YouTrackTocService");
+                serviceConfig.SetDisplayName("YouTrackToc Service");
+                serviceConfig.SetDescription("YouTrackToc Service to create table of content in YouTrack automatically.");
+                serviceConfig.RunAsLocalSystem();
+
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
---
+++ YouTrackToc/TableOfContentService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTrackToc
+{
+    class TableOfContentService
+    {
+
+        public bool Start()
+        {
+            Console.WriteLine("Started");
+
+            return true;
+        }
+
+        public TableOfContentService()
+        {
+            Console.WriteLine("Je ne comprend pas :'(");
+        }
+
+        public bool Stop()
+        {
+            Console.WriteLine("Stoped");
+            return true;
+        }
+
+    }
+}
